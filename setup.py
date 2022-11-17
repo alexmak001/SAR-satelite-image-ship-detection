8ece1a8004f934d04b1dcb2358ac17cc31c33173
ngseann <n.seanng@gmail.com>
2022-11-17 14:21:16 -0800

    fixed data download script

--- setup.py
+++ setup.py
@@ -6,27 +6,23 @@ import shutil
 
 # down load data, create new directory called data
 
+# need to change this when make new data zip
 url = 'https://drive.google.com/uc?id=10sxUzJ3BgAFUKx9lYMuTXTJdq8yGYkvy'
 output = 'data_download.zip'
 
 gdown.download(url, output, quiet=False)
-
-os.system('unzip data_download')
-
 # make data folder
 os.system('mkdir data')
+# move zip file into 
+os.system(r'mv data_download.zip data')
 
-# move unzipped data to data folder
-# for windows
-# shutil.move(r'Annotations_sub', r'data')
-# shutil.move(r'Main', r'data')
-# shutil.move(r'JPEGImages_sub', r'data')
+# change directory to data folder
+os.chdir('data')
 
-# for linux
-os.system(r'mv Annotations_sub data')
-os.system(r'mv Main data')
-os.system(r'mv JPEGImages_sub data')
+# unzip data download
+os.system('unzip data_download.zip')
 
+os.chdir('..')
 
 
 # move xml script inside and run it to create 
