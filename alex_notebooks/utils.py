06902a4fdb8ee55949358f1624c8681efa619450
Alex <alexm7701@gmail.com>
2023-01-29 19:10:15 -0800

    test

---
+++ alex_notebooks/utils.py
@@ -0,0 +1,277 @@
+import os
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.patches as patches
+from bs4 import BeautifulSoup
+
+from PIL import Image
+import cv2
+import numpy as np
+import time
+import torch
+import torchvision
+from torch.utils.data import Dataset
+from matplotlib import pyplot as plt
+import os
+import random
+
+
+# helper function for dataset
+def generate_box(obj):
+    
+    xmin = float(obj.find('xmin').text)
+    ymin = float(obj.find('ymin').text)
+    xmax = float(obj.find('xmax').text)
+    ymax = float(obj.find('ymax').text)
+    
+    return [xmin, ymin, xmax, ymax]
+
+def generate_label(obj):
+    # only have ships
+    return 1 
+
+def generate_target(file): 
+    with open(file) as f:
+        data = f.read()
+        soup = BeautifulSoup(data, "xml")
+        objects = soup.find_all("object")
+
+        num_objs = len(objects)
+
+        boxes = []
+        labels = []
+        for i in objects:
+            boxes.append(generate_box(i))
+            labels.append(generate_label(i))
+
+
+        boxes = torch.as_tensor(boxes, dtype=torch.float32) 
+        labels = torch.as_tensor(labels, dtype=torch.int64) 
+        
+        target = {}
+
+        
+        target["boxes"] = boxes
+        target["labels"] = labels
+        
+        return target
+
+def plot_image_from_output(img, annotation):
+    
+    img = img.permute(1,2,0)
+    
+    fig,ax = plt.subplots(1)
+    ax.imshow(img)
+    
+    for idx in range(len(annotation["boxes"])):
+        xmin, ymin, xmax, ymax = annotation["boxes"][idx]
+
+        if annotation['labels'][idx] == 0 :
+            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='r',facecolor='none')
+        
+        elif annotation['labels'][idx] == 1 :
+            
+            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='g',facecolor='none')
+            
+        else :
+        
+            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='orange',facecolor='none')
+        
+        ax.add_patch(rect)
+
+    plt.show()
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+
+def plot_image_from_output2(img, annotation):
+    
+    img = img.cpu().permute(1,2,0)
+    
+    rects = []
+
+    for idx in range(len(annotation["boxes"])):
+        xmin, ymin, xmax, ymax = annotation["boxes"][idx]
+
+        if annotation['labels'][idx] == 0 :
+            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='r',facecolor='none')
+        
+        elif annotation['labels'][idx] == 1 :
+            
+            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='r',facecolor='none')
+            
+        else :
+        
+            rect = patches.Rectangle((xmin,ymin),(xmax-xmin),(ymax-ymin),linewidth=1,edgecolor='orange',facecolor='none')
+
+        rects.append(rect)
+
+    return img, rects
+
+def make_prediction(model, img, threshold):
+    model.eval()
+    preds = model(img)
+    for id in range(len(preds)) :
+        idx_list = []
+
+        for idx, score in enumerate(preds[id]['scores']) :
+            if score > threshold : #select idx which meets the threshold
+                idx_list.append(idx)
+
+        preds[id]['boxes'] = preds[id]['boxes'][idx_list]
+        preds[id]['labels'] = preds[id]['labels'][idx_list]
+        preds[id]['scores'] = preds[id]['scores'][idx_list]
+
+
+    return preds
+
+
+def get_batch_statistics(outputs, targets, iou_threshold):
+    """ Compute true positives, predicted scores and predicted labels per sample """
+    batch_metrics = []
+    for sample_i in range(len(outputs)):
+
+        if outputs[sample_i] is None:
+            continue
+
+        output = outputs[sample_i] # predict
+        # pred_boxes = output['boxes']
+        # pred_scores = output['scores']
+        # pred_labels = output['labels']
+
+        true_positives = torch.zeros(output['boxes'].shape[0])   
+ 
+        annotations = targets[sample_i]  # actual
+        target_labels = annotations['labels'] if len(annotations) else []
+        if len(annotations):    # len(annotations) = 3
+            detected_boxes = []
+            target_boxes = annotations['boxes']
+
+            for pred_i, (pred_box, pred_label) in enumerate(zip(output['boxes'], output['labels'])): # 예측값에 대해서..
+
+                # If targets are found break
+                if len(detected_boxes) == len(target_labels): # annotations -> target_labels
+                    break
+
+                # Ignore if label is not one of the target labels
+                if pred_label not in target_labels:
+                    continue
+
+                iou, box_index = bbox_iou(pred_box.unsqueeze(0), target_boxes).max(0)   # box_index : 실제 어떤 바운딩 박스랑 IoU 가 가장 높은지 index
+                if iou >= iou_threshold and box_index not in detected_boxes: 
+                    true_positives[pred_i] = 1
+                    detected_boxes += [box_index] 
+        batch_metrics.append([true_positives, output['scores'], output['labels']])
+    return batch_metrics
+
+def bbox_iou(box1, box2, x1y1x2y2=True):
+    """
+    Returns the IoU of two bounding boxes
+    """
+    if not x1y1x2y2:
+        # Transform from center and width to exact coordinates
+        b1_x1, b1_x2 = box1[:, 0] - box1[:, 2] / 2, box1[:, 0] + box1[:, 2] / 2
+        b1_y1, b1_y2 = box1[:, 1] - box1[:, 3] / 2, box1[:, 1] + box1[:, 3] / 2
+        b2_x1, b2_x2 = box2[:, 0] - box2[:, 2] / 2, box2[:, 0] + box2[:, 2] / 2
+        b2_y1, b2_y2 = box2[:, 1] - box2[:, 3] / 2, box2[:, 1] + box2[:, 3] / 2
+    else:
+        # Get the coordinates of bounding boxes
+        b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+        b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+
+    # get the corrdinates of the intersection rectangle
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+    # Intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+    # Union Area
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area + 1e-16)
+
+    return iou
+
+def ap_per_class(tp, conf, pred_cls, target_cls):
+    """ Compute the average precision, given the recall and precision curves.
+    Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
+    # Arguments
+        tp:    True positives (list).
+        conf:  Objectness value from 0-1 (list).
+        pred_cls: Predicted object classes (list).
+        target_cls: True object classes (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+
+    # Sort by objectness
+    i = torch.argsort(-conf)
+    tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
+
+    # Find unique classes
+    unique_classes = torch.unique(target_cls)   
+
+    # Create Precision-Recall curve and compute AP for each class
+    ap, p, r = [], [], []
+    for c in unique_classes:
+        i = pred_cls == c
+        n_gt = (target_cls == c).sum()  # Number of ground truth objects
+        n_p = i.sum()  # Number of predicted objects
+
+        if n_p == 0 and n_gt == 0:
+            continue
+        elif n_p == 0 or n_gt == 0:
+            ap.append(0)
+            r.append(0)
+            p.append(0)
+        else:
+            # Accumulate FPs and TPs
+            fpc = torch.cumsum(1 - tp[i],-1)
+            tpc = torch.cumsum(tp[i],-1)
+
+            # Recall
+            recall_curve = tpc / (n_gt + 1e-16)
+            r.append(recall_curve[-1])
+
+            # Precision
+            precision_curve = tpc / (tpc + fpc)
+            p.append(precision_curve[-1])
+
+            # AP from recall-precision curve
+            ap.append(compute_ap(recall_curve, precision_curve))
+
+    # Compute F1 score (harmonic mean of precision and recall)
+    p, r, ap = torch.tensor(np.array(p)), torch.tensor(np.array(r)), torch.tensor(np.array(ap))
+    f1 = 2 * p * r / (p + r + 1e-16)
+
+    return p, r, ap, f1, unique_classes
+
+def compute_ap(recall, precision):
+    """ Compute the average precision, given the recall and precision curves.
+    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+    # Arguments
+        recall:    The recall curve (list).
+        precision: The precision curve (list).
+    # Returns
+        The average precision as computed in py-faster-rcnn.
+    """
+    # correct AP calculation
+    # first append sentinel values at the end
+    mrec = np.concatenate(([0.0], recall, [1.0]))
+    mpre = np.concatenate(([0.0], precision, [0.0]))
+
+    # compute the precision envelope
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+    # to calculate area under PR curve, look for points
+    # where X axis (recall) changes value
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+    # and sum (\Delta recall) * prec
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap
