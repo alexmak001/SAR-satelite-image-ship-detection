a244c78abe0d847e5a884e636b53df98ee55f138
Alex <alexm7701@gmail.com>
2023-02-14 19:19:22 -0800

    a

---
+++ shipcounter.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+import sys
+
+import ee
+import geemap
+import rasterio
+import cv2
+from PIL import Image
+
+#from osgeo import gdal
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+from datetime import date
+from datetime import timedelta
+import joblib
+import numpy as np
+
+import ast
+
+import joblib
+
+
+def ship_counter(place_coords, start_date, end_date):
+
+
+
+
+    return 
