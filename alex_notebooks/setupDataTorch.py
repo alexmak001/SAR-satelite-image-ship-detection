96b0bbd083b2ef0e42931b65c519823635167519
Alex <alexm7701@gmail.com>
2023-02-20 18:33:34 -0800

    data set up

--- alex_notebooks/setupDataTorch.py
+++ alex_notebooks/setupDataTorch.py
@@ -25,3 +25,24 @@ os.chdir('data')
 os.system('unzip data_download.zip')
 os.system("rm data_download.zip")
 os.chdir('..')
+
+# make train_yolo and test_yolo to move yolo annotations into seperate folders
+
+os.system('mkdir data/train_yolo')
+os.system('mkdir data/test_yolo')
+
+all_files = list(sorted(os.listdir("data/annotations_yolo/")))
+
+for file in all_files:
+    num = int(file[:2])
+    original = "data/annotations_yolo/" + file
+        
+    
+    if num < 12:
+        target = "data/train_yolo/" + file
+        shutil.copy(original, target)
+    else:
+        target = "data/test_yolo/" + file
+        shutil.copy(original, target)
+        
+    
