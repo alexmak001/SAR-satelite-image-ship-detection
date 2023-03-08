326c44be1c4c511fd9509926d3cc5400ac97716e
Alex <alexm7701@gmail.com>
2023-03-07 22:02:17 -0800

    nice notebook

--- alex_notebooks/utility.py
+++ alex_notebooks/utility.py
@@ -305,64 +305,6 @@ def calc_test_stats(val_loader, model, threshold, device):
 
     return precision, recall, AP, f1, mAP
 
-def plot_large_image(model,image_path,threshold,save_path,device):
-    """
-    model: pytorch 
-    image_path: path tif file
-    threshold: confidence threshold for model
-    save_path: path to save plot (must be jpg), if none will not save
-    """
-
-    # split image and save size for future
-    split_image, _ = image_splitter(image_path)
-    split_size = split_image.shape[:2]
-
-    # flatten image and format to work with pytroch device
-    flattened = np.reshape(split_image, (-1, split_image.shape[2], split_image.shape[3]))
-    torch_split_img = np.array(flattened)
-    torch_split_img = torch.tensor(torch_split_img,dtype=torch.float32)
-    torch_split_img = torch.unsqueeze(torch_split_img, dim=0)
-    torch_split_img = torch_split_img.permute(1,0,2,3)
-    torch_split_img = torch_split_img.to(device)
-
-    # predict bounding boxes
-    pred = model(torch_split_img)
-    # reshape to match image split
-    pred = np.reshape(pred,split_size)
-
-
-    # Create a 3x3 plot with a shared axis
-    fig, axs = plt.subplots(split_size[0], split_size[1], sharex=True, sharey=True, figsize=(6, 6))
-
-    # Loop over the images and annotations and plot them on the corresponding subplot
-    for i in range(split_size[0]):
-        for j in range(split_size[1]):
-            # Plot the image
-            axs[i, j].imshow(split_image[i, j], cmap='gray')
-            
-            # Remove the axis labels and ticks
-            axs[i, j].set_xticks([])
-            axs[i, j].set_yticks([])
-            axs[i, j].axis('off')
-            # Loop over the annotations and plot them as bounding boxes
-            curPred = pred[i, j]
-            for k in range(len(curPred["scores"])):
-                # checks if above threshold
-                if curPred["scores"][k] > threshold:
-                    annotation = curPred['boxes'][k]
-                    annotation = annotation.cpu().detach().numpy()
-                    left, upper, right, lower = annotation
-                    width, height = right - left, lower - upper
-                    rect = plt.Rectangle((left, upper), width, height, linewidth=1, edgecolor='r', facecolor='none')
-                    axs[i, j].add_patch(rect)
-
-    # Remove the space between the images
-    plt.subplots_adjust(wspace=0, hspace=0)
-    if save_path:
-        plt.savefig(save_path,quality=100,dpi=500)
-    # Show the plot
-    plt.show()
-
 def image_splitter(img_fp):
     """
     Takes the image filepath returns m x n array of the subimages
@@ -428,12 +370,13 @@ def image_splitter(img_fp):
     
     return split, img_name
 
-def plot_large_image(model,image_path,threshold,save_path):
+def plot_large_image(model,image_path,threshold,save_path,device):
     """
     model: pytorch 
     image_path: path tif file
     threshold: confidence threshold for model
     save_path: path to save plot (must be jpg), if none will not save
+    device: device
     """
 
     # split image and save size for future
@@ -487,3 +430,385 @@ def plot_large_image(model,image_path,threshold,save_path):
         plt.savefig(save_path,quality=100,dpi=500)
     # Show the plot
     plt.show()
+
+
+def get_metric(model,datalaoder,threshold,device):
+    """
+    Not to be used with YOLO
+    """
+
+    t1 = time.time()
+    labels = []
+    preds_adj_all = []
+    annot_all = []
+
+    for im, annot in datalaoder:
+        im = list(img.to(device) for img in im)
+        #annot = [{k: v.to(device) for k, v in t.items()} for t in annot]
+
+        for t in annot:
+            labels += t['labels']
+
+        with torch.no_grad():
+            preds_adj = make_prediction(model, im, threshold)
+            preds_adj = [{k: v.to(torch.device('cpu')) for k, v in t.items()} for t in preds_adj]
+            preds_adj_all.append(preds_adj)
+            annot_all.append(annot)
+            
+    
+    sample_metrics = []
+    for batch_i in range(len(preds_adj_all)):
+        sample_metrics += get_batch_statistics(preds_adj_all[batch_i], annot_all[batch_i], iou_threshold=0.5) 
+    
+    true_positives, pred_scores, pred_labels = [torch.cat(x, 0) for x in list(zip(*sample_metrics))] 
+    precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, torch.tensor(labels))
+    mAP = torch.mean(torch.tensor(AP,dtype=torch.float64))
+    t2 = time.time()
+    t3 = t2-t1
+    result = "Precision: {0:.3f} \n Recall: {1:.3f} \n AP: {2:.3f} \n F1: {3:.3f} \n mAP {4:.3f} \n time: {5:.3f} \n threshold: {6:}".format(precision.item(), recall.item(), AP.item(), f1.item(),mAP.item(), t3,threshold)
+    #print(result)
+    print(threshold + "done")
+    return [precision.item(), recall.item(), AP.item(), f1.item(),mAP.item(), t3, threshold]
+
+def plot_ground_truth_and_predictions(model, data_loader, device,threshold, n,save_path):
+    """
+    For RetinaNet and Faster R-CNN
+    model: model to predict
+    data_loader: test_data loader to use
+    device: the device to use
+    threshold: confidence value to threshold a prediction
+    n: random into scroll on images
+    save_path: save path of images, must end in .jpg
+
+    returns: none
+    prints a random image from data_loader with both its ground truth and the models predictions
+    """
+    # Get a batch of validation data
+    data_loader = iter(data_loader)
+    #images, targets = next(data_loader)
+    images, targets = next(data_loader)
+    
+    for i in range(n):
+        images, targets = next(data_loader)
+
+    images = images[0]
+    # Send the images and targets to the device
+    images = images.to(device)
+    targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+    images = torch.unsqueeze(images, dim=0)
+    # Run the model on the images
+    outputs = model(images)
+
+
+    # Loop over the images in the batch
+    for i in range(len(images)):
+        # Get the image, target boxes, and predicted boxes for this image
+        print(images[i].shape)
+        image = images[i].cpu().permute(1, 2, 0).numpy()
+        print(image.shape)
+        target_boxes = targets[i]['boxes'].cpu().numpy()
+
+        # grabs all ids that are above the threshold value
+        idx_list = []
+        for idx, score in enumerate(outputs[0]['scores']) :
+            if score > threshold : #select idx which meets the threshold
+                idx_list.append(idx)
+
+        predicted_boxes = outputs[i]['boxes'][idx_list].detach().cpu().numpy()
+
+        # Create a figure with two subplots
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+        # Plot the ground truth boxes on the first subplot
+        axs[0].imshow(image,cmap="gray")
+        for box in target_boxes:
+            rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1],
+                                     linewidth=2, edgecolor='g', facecolor='none')
+            axs[0].add_patch(rect)
+        axs[0].set_title('Ground Truth')
+        axs[0].set_xticks([])
+        axs[0].set_yticks([])
+
+        # Plot the predicted boxes on the second subplot
+        axs[1].imshow(image,cmap="gray")
+        for box in predicted_boxes:
+            rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1],
+                                     linewidth=2, edgecolor='r', facecolor='none')
+            axs[1].add_patch(rect)
+        axs[1].set_title('Predictions')
+        axs[1].set_xticks([])
+        axs[1].set_yticks([])
+        # Show the plot
+        if save_path:
+            plt.savefig(save_path,quality=100,dpi=500)
+        plt.show()
+
+def plot_ground_truth_and_predictions_YOLO(model, data_loader, device,threshold, n,save_path):
+    """
+    For Yolo Model only!
+    model: model to predict
+    data_loader: test_data loader to use
+    device: the device to use
+    threshold: confidence value to threshold a prediction
+    n: random into scroll on images
+    save_path: save path of images, must end in .jpg
+
+    returns: none
+    prints a random image from data_loader with both its ground truth and the models predictions
+    """
+    # Get a batch of validation data
+
+    data_loader = iter(data_loader)
+    #images, targets = next(data_loader)
+    images, targets = next(data_loader)
+    
+    for i in range(n):
+        images, targets = next(data_loader)
+
+    # image on cpu for yolo
+    images = torch.squeeze(images[0],0).cpu().numpy()
+
+    # images = images[0]
+    # # Send the images and targets to the device
+    # #images = images.to(device)
+    # targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+    # images = torch.unsqueeze(images, dim=0)
+    # Run the model on the images
+    outputs = model(images)
+
+    # YOLO conversion
+    outputs = outputs.pred[0]
+
+    # Create a dictionary with keys 'boxes', 'labels', and 'scores'
+    outputs = {
+        'boxes': outputs[:, :4],  # only take the first four columns of the tensor
+        'labels': outputs[:, 5].long()+1,
+        'scores': outputs[:, 4]
+    }
+    outputs = [outputs]
+
+    # Loop over the images in the batch
+    for i in range(len(images)):
+        # Get the image, target boxes, and predicted boxes for this image
+        # make image 800, 800, 1
+        image = images[ :, :, np.newaxis]
+        target_boxes = targets[i]['boxes'].cpu().numpy()
+
+        # grabs all ids that are above the threshold value
+        idx_list = []
+        for idx, score in enumerate(outputs[0]['scores']) :
+            if score > threshold : #select idx which meets the threshold
+                idx_list.append(idx)
+
+        predicted_boxes = outputs[i]['boxes'][idx_list].detach().cpu().numpy()
+
+        # Create a figure with two subplots
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+        # Plot the ground truth boxes on the first subplot
+        axs[0].imshow(image,cmap="gray")
+        for box in target_boxes:
+            rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1],
+                                     linewidth=2, edgecolor='g', facecolor='none')
+            axs[0].add_patch(rect)
+        axs[0].set_title('Ground Truth')
+        axs[0].set_xticks([])
+        axs[0].set_yticks([])
+
+        # Plot the predicted boxes on the second subplot
+        axs[1].imshow(image,cmap="gray")
+        for box in predicted_boxes:
+            rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1],
+                                     linewidth=2, edgecolor='r', facecolor='none')
+            axs[1].add_patch(rect)
+        axs[1].set_title('Predictions')
+        axs[1].set_xticks([])
+        axs[1].set_yticks([])
+        # Show the plot
+        
+        if save_path:
+            plt.savefig(save_path,quality=100,dpi=500)
+        plt.show()
+
+def make_prediction_yolo(model, img, threshold):
+    model.eval()
+    preds = model(img)
+    
+    preds = preds.pred[0]
+
+    # Create a dictionary with keys 'boxes', 'labels', and 'scores'
+    preds = {
+        'boxes': preds[:, :4],  # only take the first four columns of the tensor
+        'labels': preds[:, 5].long()+1,
+        'scores': preds[:, 4]
+    }
+    preds = [preds]
+    
+    for id in range(len(preds)) :
+        idx_list = []
+
+        for idx, score in enumerate(preds[id]['scores']) :
+            if score > threshold : #select idx which meets the threshold
+                idx_list.append(idx)
+
+        preds[id]['boxes'] = preds[id]['boxes'][idx_list]
+        preds[id]['labels'] = preds[id]['labels'][idx_list]
+        preds[id]['scores'] = preds[id]['scores'][idx_list]
+
+
+    return preds
+
+def get_metric_YOLO(model, dataloader, threshold):
+    t1 = time.time()
+    labels = []
+    preds_adj_all = []
+    annot_all = []
+
+    for im, annot in dataloader:
+        
+        for t in annot:
+            
+            labels += t['labels']
+        
+        with torch.no_grad():
+            ### make a new function for yolo, need to move to cpu first
+            # will slow down time significantly
+            im = torch.squeeze(im[0],0).cpu().numpy()
+            preds_adj = make_prediction_yolo(model, im, threshold)
+            preds_adj = [{k: v.to(torch.device('cpu')) for k, v in t.items()} for t in preds_adj]
+            preds_adj_all.append(preds_adj)
+            annot_all.append(annot)
+    
+    sample_metrics = []
+    for batch_i in range(len(preds_adj_all)):
+        sample_metrics += get_batch_statistics(preds_adj_all[batch_i], annot_all[batch_i], iou_threshold=0.5) 
+    true_positives, pred_scores, pred_labels = [torch.cat(x, 0) for x in list(zip(*sample_metrics))] 
+    precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, torch.tensor(labels))
+    mAP = torch.mean(torch.tensor(AP,dtype=torch.float64))
+    t2 = time.time()
+    t3 = t2-t1
+    result = "Precision: {0:.3f} \n Recall: {1:.3f} \n AP: {2:.3f} \n F1: {3:.3f} \n mAP {4:.3f} \n time: {5:.3f} \n threshold: {6:}".format(precision.item(), recall.item(), AP.item(), f1.item(),mAP.item(), t3,threshold)
+    print(result)
+    return [precision.item(), recall.item(), AP.item(), f1.item(),mAP.item(), t3, threshold]
+
+def read_file(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+# array of all test annotation with ships 
+allShipsTest = os.listdir("test_yolo/")
+allShipsTest = [ship[:-4] for ship in allShipsTest]
+# array of all inshore annotations
+allInshore = read_file("main/test_inshore.txt")
+# array of all offshore annotations
+allOffshore = read_file("main/test_offshore.txt")
+set1 = set(allShipsTest)
+set2 = set(allInshore)
+set3 = set(allOffshore)
+
+inshoreShipTest = list(set1.intersection(set2))
+offshoreShipTest = list(set1.intersection(set3))
+
+class ShipDataset:
+    def __init__(self, path, transform=None):
+        self.path = path
+        if path == "no_ship":
+            self.files = "chui"
+        elif path == "test":
+            self.files = allShipsTest
+        elif path == "in":
+            self.files = inshoreShipTest
+        elif path == "off":
+            self.files = offshoreShipTest
+        else:
+            self.files = list(sorted(os.listdir(path)))
+        
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        if self.path in ["no_ship","test","in","off"]:
+            file_image = self.files[idx] + '.jpg'
+            file_label = self.files[idx] + '.xml'
+        else:
+            file_image = self.files[idx][:-3] + 'jpg'
+            file_label = self.files[idx][:-3] + 'xml'
+
+        img_path = os.path.join("images/", file_image)
+        label_path = os.path.join("annotations/", file_label)
+        #print(img_path,label_path)
+        
+        # Read an image with OpenCV, gray scale
+        image = cv2.imread(img_path,0)
+        
+        image = image/255.0
+        target = generate_target(label_path)
+
+        
+        if self.transform:
+            transformed = self.transform(image = image, bboxes = target['boxes'], labels = target['labels'])
+            image = torch.Tensor(transformed['image'])
+            target = {'boxes':torch.Tensor(transformed['bboxes']).reshape(-1,4), 'labels':torch.Tensor(transformed['labels'])}
+        else:
+            image = torch.tensor(image,dtype=torch.float32)
+        
+        image = torch.unsqueeze(image, dim=0)
+            
+        return image, target 
+
+class ShipDataset_YOLO:
+    def __init__(self, path, transform=None):
+        self.path = path
+        if path == "no_ship":
+            self.files = "chui"
+        elif path == "test":
+            self.files = allShipsTest
+        elif path == "in":
+            self.files = inshoreShipTest
+        elif path == "off":
+            self.files = offshoreShipTest
+        else:
+            self.files = list(sorted(os.listdir(path)))
+        
+        self.transform = transform
+        
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        if self.path in ["no_ship","test","in","off"]:
+            file_image = self.files[idx] + '.jpg'
+            file_label = self.files[idx] + '.xml'
+        else:
+            file_image = self.files[idx][:-3] + 'jpg'
+            file_label = self.files[idx][:-3] + 'xml'
+
+        img_path = os.path.join("images/", file_image)
+        label_path = os.path.join("annotations/", file_label)
+        #print(img_path,label_path)
+        
+        # Read an image with OpenCV, gray scale
+        image = cv2.imread(img_path,0)
+        
+        image = image#/255.0
+        target = generate_target(label_path)
+
+        
+        if self.transform:
+            transformed = self.transform(image = image, bboxes = target['boxes'], labels = target['labels'])
+            image = torch.Tensor(transformed['image'])
+            target = {'boxes':torch.Tensor(transformed['bboxes']).reshape(-1,4), 'labels':torch.Tensor(transformed['labels'])}
+        else:
+            image = torch.tensor(image,dtype=torch.float32)
+        
+        image = torch.unsqueeze(image, dim=0)
+            
+        return image, target 
+
